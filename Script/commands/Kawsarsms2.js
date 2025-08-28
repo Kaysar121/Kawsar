029cc8ca5bacf646355ea2fc6a909858cef96346
Kaysar121 <opkaysaryt2@gmail.com>
2025-08-28 15:34:28 +0600

    Update Kawsarsms2.js

--- Script/commands/Kawsarsms2.js
+++ Script/commands/Kawsarsms2.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "sms",
+  name: "sms2",
   version: "2.0.0",
   hasPermssion: 0,
   credits: "—͟͟͞͞𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", //ক্রেডিট চেঞ্জ করলে এপিআই বন্ধ করে দেব।
