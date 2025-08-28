296afcd4a0bb226911ed0701fb4ac9aa96a7bf81
Kaysar121 <opkaysaryt2@gmail.com>
2025-08-28 18:49:27 +0600

    Update sms.js

--- Script/commands/sms.js
+++ Script/commands/sms.js
@@ -20,21 +20,21 @@ module.exports.run = async ({ api, event, args }) => {
   if (number === "off") {
     if (bombingFlags[threadID]) {
       bombingFlags[threadID] = false;
-      return api.sendMessage("✅ SMS BOMBER 1 Offed ।", threadID);
+      return api.sendMessage("✅ SMS বোম্বার বন্ধ করা হয়েছে।", threadID);
     } else {
       return api.sendMessage("❗এই থ্রেডে কোন বোম্বিং চলছিল না।", threadID);
     }
   }
  
   if (!/^01[0-9]{9}$/.test(number)) {
-    return api.sendMessage("•┄┅════❁🌺❁════┅┄•\n\nSMS BOMBER 1\n\nব্যবহার:\n.sms number\n\n(BD Number Only)\n\n•┄┅════❁🌺❁════┅┄•", threadID);
+    return api.sendMessage("•┄┅════❁🌺❁════┅┄•\n\n☠️••SMS BOMBER BY —͟͟͞͞𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️💣\n\nব্যবহার:\n/sms 01xxxxxxxxx\n\n(বাংলাদেশি নাম্বার দিন, শুধু মজার জন্য ব্যবহার করুন)\n\n•┄┅════❁🌺❁════┅┄•", threadID);
   }
  
   if (bombingFlags[threadID]) {
-    return api.sendMessage("❗sms bomber 1 already on off korte .sms off", threadID);
+    return api.sendMessage("❗এই থ্রেডে ইতিমধ্যে বোম্বিং চলছে! বন্ধ করতে /sms off", threadID);
   }
  
-  api.sendMessage(`✅ SMS BOMBER 1 ON  ${number} ...\nOff korte .sms off`, threadID);
+  api.sendMessage(`✅ SMS বোম্বিং শুরু হয়েছে ${number} নম্বরে...\nবন্ধ করতে /sms off`, threadID);
  
   bombingFlags[threadID] = true;
  
